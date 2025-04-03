128715029a88022ee81aacbbc4071457c07849ed
Nick Charney Kaye <charneykaye@gmail.com>
2025-04-02 21:56:43 -0700

    ship wraps off one side of screen into the other

--- models/ships/ship_test.gd
+++ models/ships/ship_test.gd
@@ -3,12 +3,21 @@ extends RigidBody2D
 
 const FORCE_AMOUNT = 1000
 const LINEAR_DAMP = 10
+const SCREEN_WRAPAROUND_MARGIN = 10
 
+var screen_size: Vector2
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	set_linear_damp(LINEAR_DAMP)
+	get_tree().root.size_changed.connect(_on_viewport_resize)
+	_on_viewport_resize()
 	pass # Replace with function body.
+
+
+func _on_viewport_resize() -> void:
+	screen_size = get_viewport_rect().size
+	pass
 
  
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -26,5 +35,15 @@ func _process(delta: float) -> void:
 	# get angle from linear velocity and rotate the ship halfway towards that target
 	var target_angle = linear_velocity.angle()
 	rotation = target_angle
+	
+	# if ship leaves the screen, wrap around
+	if position.x < -SCREEN_WRAPAROUND_MARGIN:
+		position.x += screen_size.x + SCREEN_WRAPAROUND_MARGIN * 2
+	elif position.x > screen_size.x + SCREEN_WRAPAROUND_MARGIN:
+		position.x -= screen_size.x  + SCREEN_WRAPAROUND_MARGIN * 2
+	elif position.y < -SCREEN_WRAPAROUND_MARGIN:
+		position.y += screen_size.y + SCREEN_WRAPAROUND_MARGIN * 2
+	elif position.y > screen_size.y + SCREEN_WRAPAROUND_MARGIN:
+		position.y -= screen_size.y + SCREEN_WRAPAROUND_MARGIN * 2
 			
 	pass
