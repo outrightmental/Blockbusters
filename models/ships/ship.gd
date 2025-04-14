9f202677a06e4caf210c1aa6ce01d50b6230b740
Nick Charney Kaye <charneykaye@gmail.com>
2025-04-14 10:23:08 -0700

    2 players

--- models/ships/ship.gd
+++ models/ships/ship.gd
@@ -2,16 +2,38 @@ extends RigidBody2D
 
 
 const FORCE_AMOUNT: int       = 1000
-const LINEAR_DAMP: int        = 10
+const LINEAR_DAMP: int        = 1
 const SCREEN_WRAP_MARGIN: int = 15
 
 var screen_size: Vector2
+
+var input_mapping: Dictionary = {
+									"left": "ui_left",
+									"right": "ui_right",
+									"up": "ui_up",
+									"down": "ui_down"
+								}
+var input_mapping_format: Dictionary = {
+									"left": "p%d_left",
+									"right": "p%d_right",
+									"up": "p%d_up",
+									"down": "p%d_down"
+								}
+
+@export var player_num: int = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	set_linear_damp(LINEAR_DAMP)
 	get_tree().root.size_changed.connect(_on_viewport_resize)
 	_on_viewport_resize()
+	# Set up input mapping for player
+	for key in input_mapping.keys():
+		var action_name: String = input_mapping_format[key] % player_num
+		if InputMap.has_action(action_name):
+			input_mapping[key] = action_name
+		else:
+			print("Input action not found: ", action_name)
 	pass # Replace with function body.
 
 
@@ -22,14 +44,14 @@ func _on_viewport_resize() -> void:
  
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
-	# listen for ASDW keys and apply acceleration to this rigid body 2d physics object
-	if Input.is_action_pressed("ui_right"):
+	# Apply forces based on input mapping
+	if Input.is_action_pressed(input_mapping["right"]):
 		apply_central_force(Vector2(FORCE_AMOUNT, 0))
-	if Input.is_action_pressed("ui_left"):
+	if Input.is_action_pressed(input_mapping["left"]):
 		apply_central_force(Vector2(-FORCE_AMOUNT, 0))
-	if Input.is_action_pressed("ui_up"):
+	if Input.is_action_pressed(input_mapping["up"]):
 		apply_central_force(Vector2(0, -FORCE_AMOUNT))
-	if Input.is_action_pressed("ui_down"):
+	if Input.is_action_pressed(input_mapping["down"]):
 		apply_central_force(Vector2(0, FORCE_AMOUNT))
 		
 	# get angle from linear velocity and rotate the ship halfway towards that target
