6b895743780f274a029c8be06e1955a6ac458672
Nick Charney Kaye <charneykaye@gmail.com>
2025-04-14 13:52:01 -0700

    Check out this first build of the Coffee Table Asteroids 2-player game mechanics. Player One moves with ADWS and Player Two moves with JLIK. https://drive.google.com/file/d/1UJhFm0typqMPvkH9Z6c-A21QTC-gOuA9/view?usp=drive_link

--- models/ships/ship.gd
+++ models/ships/ship.gd
@@ -1,10 +1,9 @@
 extends RigidBody2D
 
-
-const FORCE_AMOUNT: int       = 500
-const LINEAR_DAMP: int        = 1
-const SCREEN_WRAP_MARGIN: int = 15
-
+const FORCE_AMOUNT: int             = 500
+const LINEAR_DAMP: int              = 1
+const SCREEN_WRAP_MARGIN: int       = 15
+const TARGET_ROTATION_FACTOR: float = 10
 var screen_size: Vector2
 
 var input_mapping: Dictionary = {
@@ -13,28 +12,28 @@ var input_mapping: Dictionary = {
 									"up": "ui_up",
 									"down": "ui_down"
 								}
-const input_mapping_format: Dictionary = {
-									"left": "p%d_left",
-									"right": "p%d_right",
-									"up": "p%d_up",
-									"down": "p%d_down"
-								}
 
-const dir_vectors := {
-					   "right": Vector2(1, 0),
-					   "left": Vector2(-1, 0),
-					   "up": Vector2(0, -1),
-					   "down": Vector2(0, 1),
-				   }
-
+const input_mapping_format: Dictionary =  {
+											  "left": "p%d_left",
+											  "right": "p%d_right",
+											  "up": "p%d_up",
+											  "down": "p%d_down"
+										  }
+const dir_vectors                      := {
+											  "right": Vector2(1, 0),
+											  "left": Vector2(-1, 0),
+											  "up": Vector2(0, -1),
+											  "down": Vector2(0, 1),
+										  }
 # keep track of the time when the input was pressed
-var input_start_time : float = 0.0
-
+var input_start_ticks_msec: float = 0.0
 # whether the input is pressed
-var input_pressed : bool = false
-
+var input_pressed: bool = false
 # threshold that's rotation only (strafe) before applying force
-const STRAFE_THRESHOLD: float = 0.2
+const STRAFE_THRESHOLD_MSEC: float = 500
+# fixed actual angle moves towards target angle -- used for strafe/accelerate mechanic
+var target_rotation: float = 0.0
+var actual_rotation: float = 0.0
 
 # Map player_num to textures
 var player_textures: Dictionary = {
@@ -44,6 +43,7 @@ var player_textures: Dictionary = {
 
 # Player number to identify the ship
 @export var player_num: int = 0
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -57,12 +57,12 @@ func _ready() -> void:
 			input_mapping[key] = action_name
 		else:
 			print("Input action not found: ", action_name)
-		
+
 	# Set the sprite texture based on player_num
 	if player_num in player_textures:
 		$Sprite2D.texture = player_textures[player_num]
 	else:
-		print("No texture found for player_num: ", player_num)		
+		print("No texture found for player_num: ", player_num)
 
 	pass
 
@@ -71,55 +71,49 @@ func _on_viewport_resize() -> void:
 	screen_size = get_viewport_rect().size
 	pass
 
- 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta: float) -> void:
-#	# Apply forces based on input mapping
-#	if Input.is_action_pressed(input_mapping["right"]):
-#		apply_central_force(Vector2(FORCE_AMOUNT, 0))
-#	if Input.is_action_pressed(input_mapping["left"]):
-#		apply_central_force(Vector2(-FORCE_AMOUNT, 0))
-#	if Input.is_action_pressed(input_mapping["up"]):
-#		apply_central_force(Vector2(0, -FORCE_AMOUNT))
-#	if Input.is_action_pressed(input_mapping["down"]):
-#		apply_central_force(Vector2(0, FORCE_AMOUNT))
 
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
 	# Get input vector from which keys are pressed
 	var input_vector: Vector2 = Vector2.ZERO
 	for key in dir_vectors.keys():
 		if Input.is_action_pressed(input_mapping[key]):
 			input_vector += dir_vectors[key]
-		
+
 	# Normalize the input vector to avoid faster diagonal movement
 	if input_vector.length() > 1:
 		input_vector = input_vector.normalized()
-	
+
 	# Reset input pressed state if no keys are pressed		
 	if input_vector == Vector2.ZERO:
 		input_pressed = false
-		input_start_time = 0.0
 	else:
 		# If the input vector is not zero, set the pressed state and start time
 		if not input_pressed:
 			input_pressed = true
-			input_start_time = OS.get_ticks_msec() / 1000.0
+			input_start_ticks_msec = Time.get_ticks_msec()
+
+		if Time.get_ticks_msec() - input_start_ticks_msec < STRAFE_THRESHOLD_MSEC:
+			# The time elapsed is less than the strafe threshold, so we turn without applying force
+			target_rotation = input_vector.angle()
+		else:
+			target_rotation = linear_velocity.angle()
 
 	# Apply force in the direction of the input vector
-	if input_vector != Vector2.ZERO:
-		apply_central_force(input_vector * FORCE_AMOUNT)
-		
-	# apply / rotate
-#	apply_central_force(dir_vectors[key] * FORCE_AMOUNT)
-#	rotation = linear_velocity.angle()
-		
-	# get angle from linear velocity and rotate the ship halfway towards that target
-	var target_angle: float = linear_velocity.angle()
-	rotation = target_angle
+	apply_central_force(input_vector * FORCE_AMOUNT)
 
 	# ship position wraps around the screen edges
 	position.x = wrapf(position.x, -SCREEN_WRAP_MARGIN, screen_size.x + SCREEN_WRAP_MARGIN)
 	position.y = wrapf(position.y, -SCREEN_WRAP_MARGIN, screen_size.y + SCREEN_WRAP_MARGIN)
-	
+
+	# Adjust the rotation towards the target angle by a factor and delta time
+	var angle_diff: float = fmod(target_rotation - actual_rotation, TAU)
+	if angle_diff > PI:
+		angle_diff -= TAU
+	elif angle_diff < -PI:
+		angle_diff += TAU
+	actual_rotation += angle_diff * TARGET_ROTATION_FACTOR * delta
+	rotation = actual_rotation
 	pass
 
 			
