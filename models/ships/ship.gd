84db1a7280d77dc4e651fa05a556e20e98d88613
Nick Charney Kaye <charneykaye@gmail.com>
2025-04-14 10:33:07 -0700

    inputs

--- models/ships/ship.gd
+++ models/ships/ship.gd
@@ -1,8 +1,8 @@
 extends RigidBody2D
 
 
-const FORCE_AMOUNT: int       = 1000
-const LINEAR_DAMP: int        = 2
+const FORCE_AMOUNT: int       = 500
+const LINEAR_DAMP: int        = 1
 const SCREEN_WRAP_MARGIN: int = 15
 
 var screen_size: Vector2
@@ -13,12 +13,19 @@ var input_mapping: Dictionary = {
 									"up": "ui_up",
 									"down": "ui_down"
 								}
-var input_mapping_format: Dictionary = {
+const input_mapping_format: Dictionary = {
 									"left": "p%d_left",
 									"right": "p%d_right",
 									"up": "p%d_up",
 									"down": "p%d_down"
 								}
+
+const dir_vectors := {
+					   "right": Vector2(1, 0),
+					   "left": Vector2(-1, 0),
+					   "up": Vector2(0, -1),
+					   "down": Vector2(0, 1),
+				   }
 
 @export var player_num: int = 0
 
@@ -57,15 +64,25 @@ func _on_viewport_resize() -> void:
  
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
-	# Apply forces based on input mapping
-	if Input.is_action_pressed(input_mapping["right"]):
-		apply_central_force(Vector2(FORCE_AMOUNT, 0))
-	if Input.is_action_pressed(input_mapping["left"]):
-		apply_central_force(Vector2(-FORCE_AMOUNT, 0))
-	if Input.is_action_pressed(input_mapping["up"]):
-		apply_central_force(Vector2(0, -FORCE_AMOUNT))
-	if Input.is_action_pressed(input_mapping["down"]):
-		apply_central_force(Vector2(0, FORCE_AMOUNT))
+#	# Apply forces based on input mapping
+#	if Input.is_action_pressed(input_mapping["right"]):
+#		apply_central_force(Vector2(FORCE_AMOUNT, 0))
+#	if Input.is_action_pressed(input_mapping["left"]):
+#		apply_central_force(Vector2(-FORCE_AMOUNT, 0))
+#	if Input.is_action_pressed(input_mapping["up"]):
+#		apply_central_force(Vector2(0, -FORCE_AMOUNT))
+#	if Input.is_action_pressed(input_mapping["down"]):
+#		apply_central_force(Vector2(0, FORCE_AMOUNT))
+
+	for key in dir_vectors.keys():
+		# Tap to rotate (strafe)
+		if Input.is_action_just_pressed(input_mapping[key]):
+			rotation = dir_vectors[key].angle()
+
+		# Hold to move and rotate to velocity
+		if Input.is_action_pressed(input_mapping[key]):
+			apply_central_force(dir_vectors[key] * FORCE_AMOUNT)
+			rotation = linear_velocity.angle()
 		
 	# get angle from linear velocity and rotate the ship halfway towards that target
 	var target_angle: float = linear_velocity.angle()
