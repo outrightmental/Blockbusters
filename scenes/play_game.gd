61b58f4d1d681dc7015c40e4f2f051e620782508
Nick Charney Kaye <charneykaye@gmail.com>
2025-04-15 07:59:39 -0700

    block clearance

--- scenes/play_game.gd
+++ scenes/play_game.gd
@@ -7,26 +7,24 @@ enum GridType {
 }
 # Spawn blocks in a grid pattern, 32 blocks wide and 18 blocks tall, starting at (16, 16) and spaced 32 pixels apart
 # The blocks are 32x32 pixels, so the grid is 1024x576 pixels
-const BLOCK_SIZE: int   = 32
-const BLOCK_CENTER: int = BLOCK_SIZE/2
-var grid: Dictionary    = {}
+const BLOCK_SIZE: int             = 32
+const BLOCK_CENTER: int           = BLOCK_SIZE/2
+const BLOCK_SHIP_CLEARANCE_RADIUS = 100
+var grid: Dictionary              = {}
 
 
 # Instantiate a models/ships/ship.gd for each player, so set player_num = 1 or 2 respectively, and Player 1 is 10% in from the left, vertical center, and Player 2 is 10% in from the right, vertical center.
 func _ready() -> void:
 	var viewport_size: Vector2 = get_viewport().get_visible_rect().size
-	var player_1 = _spawn_player(1, Vector2(viewport_size.x * 0.1, viewport_size.y * 0.5), 0)
-	var player_2 = _spawn_player(2, Vector2(viewport_size.x * 0.9, viewport_size.y * 0.5), PI)
+	var player_1               = _spawn_player(1, Vector2(viewport_size.x * 0.1, viewport_size.y * 0.5), 0)
+	var player_2               = _spawn_player(2, Vector2(viewport_size.x * 0.9, viewport_size.y * 0.5), PI)
 
-	var grid_pos : Vector2
+	#	var grid_pos: Vector2
 	for x in range(32):
 		for y in range(18):
 			if (!grid.has(x)):
 				grid[x] = {}
-			grid_pos = _grid_position(x, y)
-			var distance_to_player_1 = player_1.position.distance_to(grid_pos)
-			var distance_to_player_2 = player_2.position.distance_to(grid_pos)
-			if (distance_to_player_1 > 100 and distance_to_player_2 > 100):
+			if (_is_clear_of_all(BLOCK_SHIP_CLEARANCE_RADIUS, _grid_position(x, y), [player_1.position, player_2.position])):
 				grid[x][y] = GridType.BLOCK
 			else:
 				grid[x][y] = GridType.EMPTY
@@ -40,6 +38,13 @@ func _ready() -> void:
 				_spawn_block(_grid_position(x, y))
 
 	pass
+
+
+func _is_clear_of_all(distance: int, source: Vector2, targets: Array[Vector2]):
+	for target in targets:
+		if (source.distance_to(target) < distance):
+			return false
+	return true
 
 
 func _grid_position(x: int, y: int) -> Vector2:
