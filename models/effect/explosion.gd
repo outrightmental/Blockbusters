f58c5a6679b0cacc10997b946fd1f0c3158e22e3
Nick Charney Kaye <charneykaye@gmail.com>
2025-06-18 19:11:05 -0700

    stable but not disabling explosion collision fast enough

--- models/effect/explosion.gd
+++ models/effect/explosion.gd
@@ -1,5 +1,8 @@
 extends Node2D
 
+# Cache the collision shape
+@onready var collision_shape: CollisionShape2D = $ExplosiveArea2D/CollisionShape2D
+
 # Player number to identify the projectile
 @export var player_num: int = 0
 # Constants
@@ -17,12 +20,14 @@ var critical_radius_block: float      = 0.0
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	instantiated_at_ticks_msec = Time.get_ticks_msec()
-	explosive_radius = self.get_node("ExplosiveArea2D/CollisionShape2D").shape.radius
+	explosive_radius = collision_shape.shape.radius
 	critical_radius_ship = explosive_radius * CRITICAL_RADIUS_SHIP_RATIO
 	critical_radius_block = explosive_radius * CRITICAL_RADIUS_BLOCK_RATIO
 	$ExplosiveArea2D.body_entered.connect(_on_body_entered)
+	# Disable the collision shape so that collisions only occur at the instant of explosion
+	collision_shape.call_deferred("set_disabled", true)
 
-
+	
 # Called when another body enters the collission area
 func _on_body_entered(body: Node2D) -> void:
 	var diff: Vector2 = (body.position - position)
