outrightmental/Blockbusters

commit 954db52d75cc4ec45130b298733455321f9ca8d4

Refactor: run gameplay from Game singleton signals (#206)
- Decision to show a banner
- Pickup and gem spawning
- Game start and finish

scenes/play_game.gd
@@ -1,9 +1,9 @@
 extends Node2D
 
 # Preloaded Scenes
-const ship_scene: PackedScene  = preload('res://models/player/ship.tscn')
-const goal_scene: PackedScene  = preload('res://models/player/goal.tscn')
-const score_scene: PackedScene = preload('res://models/hud/hud_score.tscn')
+const ship_scene: PackedScene   = preload('res://models/player/ship.tscn')
+const goal_scene: PackedScene   = preload('res://models/player/goal.tscn')
+const score_scene: PackedScene  = preload('res://models/hud/hud_score.tscn')
 const block_scene: PackedScene  = preload('res://models/block/block.tscn')
 const banner_scene: PackedScene = preload('res://models/hud/hud_banner.tscn')
 # References to player goals
@@ -12,36 +12,25 @@ const banner_scene: PackedScene = preload('res://models/hud/hud_banner.tscn')
 @onready var debug_text = $DebugText
 
 # Variables
-var grid: Dictionary                     = {}
-var mesh: Dictionary                     = {}
-var block_count: int                     = 0
-var started_at_ticks_msec: int           = 0
-var spawn_next_gem_at_msec: int          = 0
-var spawn_next_pickup_at_msec: int       = 0
-var gem_dont_spawn_until_ticks_msec: int = 0
+var grid: Dictionary = {}
+var mesh: Dictionary = {}
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	started_at_ticks_msec = Time.get_ticks_msec()
-	spawn_next_gem_at_msec = started_at_ticks_msec + int(Constant.SHOW_MODAL_SEC * 1000)
-	spawn_next_pickup_at_msec = spawn_next_gem_at_msec + int(Constant.PICKUP_SPAWN_INITIAL_SEC * 1000)
 	# Setup the board based on the current input mode
 	_setup()
 	# Create the board before resetting the game (so that scores update on the board)
 	_create_board()
 	# Reset the game
-	Game.reset_game.emit()
+	Game.start_new_game.emit()
 	# Connect the game over signals after resetting the game
-	Game.goal.connect(_on_player_goal)
 	Game.player_enabled.connect(_on_player_enabled)
-	Game.over.connect(_on_game_over)
-	# Countdown and then start the game
-	Game.pause_input()
-	_show_banner(0, "READY...", "SET...")
-	await Util.delay(Constant.SHOW_MODAL_SEC)
-	Game.unpause_input()
-	# Show debug text
+	Game.finished.connect(_on_finished)
+	Game.spawn_gem.connect(_on_spawn_gem)
+	Game.spawn_pickup.connect(_on_spawn_pickup)
+	Game.show_banner.connect(_on_show_banner)
+	# Show debug text in editor only
 	if OS.has_feature("editor"):
 		Game.show_debug_text.connect(_on_show_debug_text)
 	else:
@@ -68,36 +57,14 @@ func _setup() -> void:
 			$HudPlayer2/InventoryP2.transform = Transform2D(PI/2, Vector2(1, -1), 0, Vector2(993, 88))
 
 
-# Called at a fixed rate. 'delta' is the elapsed time since the previous frame.
-func _physics_process(_delta: float) -> void:
-	# Check if it's time to spawn a gem
-	if Time.get_ticks_msec() >= spawn_next_gem_at_msec:
-		spawn_next_gem_at_msec = Time.get_ticks_msec() + int(Constant.GEM_SPAWN_EVERY_SEC * 1000)
-		_spawn_gem()
-	# Check if it's time to spawn a pickup
-	if Time.get_ticks_msec() >= spawn_next_pickup_at_msec:
-		spawn_next_pickup_at_msec = Time.get_ticks_msec() + int(Constant.PICKUP_SPAWN_EVERY_SEC * 1000)
-		_spawn_pickup(Game.InventoryItemType.PROJECTILE) # FUTURE: other types of pickups
-	pass
-
-
 # Show the game over modal for some time, then go back to main screen
-func _on_game_over(result: Game.Result) -> void:
-	gem_dont_spawn_until_ticks_msec = Time.get_ticks_msec() + 999999
-	match result:
-		Game.Result.PLAYER_1_WINS:
-			_show_banner(1, "VICTORY!")
-		Game.Result.PLAYER_2_WINS:
-			_show_banner(2, "VICTORY!")
-		Game.Result.DRAW:
-			_show_banner(0, "DRAW")
-	await Util.delay(Constant.SHOW_MODAL_SEC)
+func _on_finished() -> void:
 	_goto_scene('res://scenes/main.tscn')
 	pass
 
 
 # Spawn a banner at the given position
-func _show_banner(player_num: int, message: String, message_2: String = "") -> void:
+func _on_show_banner(player_num: int, message: String, message_2) -> void:
 	var viewport_size: Vector2 = get_viewport().get_visible_rect().size
 	match Game.mode:
 		Game.Mode.COUCH:
@@ -109,8 +76,9 @@ func _show_banner(player_num: int, message: String, message_2: String = "") -> v
 	await Util.delay(Constant.SHOW_MODAL_SEC)
 	Game.unpause_input()
 
+
 # Spawn a banner at the given position
-func _spawn_banner(player_num: int, x: float, y: float, _rotation_degrees: float, _scale: float, message: String, message_2: String = "") -> void:
+func _spawn_banner(player_num: int, x: float, y: float, _rotation_degrees: float, _scale: float, message: String, message_2: String) -> void:
 	var banner: Node = banner_scene.instantiate()
 	banner.scale = Vector2(_scale, _scale)
 	banner.position = Vector2(x, y)
@@ -120,12 +88,6 @@ func _spawn_banner(player_num: int, x: float, y: float, _rotation_degrees: float
 	banner.message_2 = message_2
 	banner.z_index = 1000
 	self.add_child(banner)
-
-
-# Called when a player scores a goal
-func _on_player_goal(player_num: int) -> void:
-	gem_dont_spawn_until_ticks_msec = Time.get_ticks_msec() + Constant.GEM_SPAWN_AFTER_SCORING_DELAY_MSEC
-	_show_banner(player_num, "GOOOAAAAL!")
 
 
 # When ship is disabled, player HUD also appears disabled #155
@@ -147,6 +109,7 @@ func _on_player_enabled(player_num: int, enabled: bool) -> void:
 # A block will only be placed if that block is above GRID_MESH_THRESHOLD in the gradient mesh
 #
 func _create_board() -> void:
+	var block_count: int         = 0
 	var block_attempt_count: int = 0
 	_generate_mesh(floor(randf() * Constant.BOARD_SEED_MAX))
 	var viewport_size: Vector2         = get_viewport().get_visible_rect().size
@@ -211,7 +174,7 @@ func _spawn_block(start_position: Vector2) -> Node:
 	return block
 
 
-func _spawn_gem() -> void:
+func _on_spawn_gem() -> void:
 	if Game.is_over:
 		return
 	if get_tree().get_node_count_in_group(Game.GEM_GROUP) >= Constant.GEM_MAX_COUNT:
@@ -219,11 +182,9 @@ func _spawn_gem() -> void:
 	var block: Block = _get_block_spawn_candidate()
 	if block != null:
 		block.add_gem()
-	else:
-		Game.gem_spawned.emit()
 
 
-func _spawn_pickup(type: Game.InventoryItemType) -> void:
+func _on_spawn_pickup(type: Game.InventoryItemType) -> void:
 	if Game.is_over:
 		return
 	if get_tree().get_node_count_in_group(Game.PICKUP_GROUP) >= Constant.PICKUP_MAX_COUNT:
@@ -231,8 +192,6 @@ func _spawn_pickup(type: Game.InventoryItemType) -> void:
 	var block: Block = _get_block_spawn_candidate()
 	if block != null:
 		block.add_pickup(type)
-	else:
-		Game.pickup_spawned.emit(type)
 
 
 # Get a random block that may have something added to it
@@ -251,8 +210,8 @@ func _get_block_spawn_candidate() -> Block:
 func _goto_scene(path: String) -> void:
 	if get_tree():
 		get_tree().change_scene_to_file(path)
-		
-		
+
+
 # Show debug text
 func _on_show_debug_text(text: String) -> void:
 	debug_text.text = text

global/constant.gd
@@ -105,5 +105,7 @@ const BOARD_SEED_F4: int                    = 167_653_873
 const BOARD_SEED_F5: int                    = 423_587_300
 const BOARD_SEED_F6: int                    = 798_647_400
 #
-# Trigonometry
-const HALF_PI: float = PI / 2
+# Banners
+const BANNER_TEXT_GOAL    := "GOOOAAAAL!"
+const BANNER_TEXT_VICTORY := "VICTORY!"
+const BANNER_TEXT_DRAW    := "DRAW"

global/game.gd
@@ -1,10 +1,7 @@
 extends Node
 
 # All available signals -- use these constants to reference them to avoid typos
-signal gem_spawned()
-signal over(result: Result)
-signal goal(player_num: int)
-signal pickup_spawned(type: InventoryItemType)
+signal finished()
 signal player_did_collect_item(player_num: int, type: InventoryItemType)
 signal player_did_launch_projectile(player_num: int)
 signal player_enabled(player_num: int, enabled: bool)
@@ -12,9 +9,11 @@ signal player_energy_updated(player_num: int, charge_ratio: float, is_available:
 signal player_inventory_updated()
 signal player_ready_updated
 signal player_score_updated()
-signal projectile_count_updated
-signal reset_game()
+signal show_banner(player_num: int, message: String, message_2: String)
 signal show_debug_text(text: String)
+signal spawn_gem()
+signal spawn_pickup(type: Game.InventoryItemType)
+signal start_new_game()
 # Group names
 const BLOCK_GROUP: StringName  = "BlockGroup"
 const GEM_GROUP: StringName    = "GemGroup"
@@ -50,14 +49,18 @@ enum Mode {
 											   2: [],
 										   }
 
-# Keeping track of the projectile count
-@export var projectiles_in_play: int = 0
 # Keep track of whether the game is over
 @export var is_over: bool = false
 # Whether the input is paused
 @export var is_input_movement_paused: bool = false
 @export var is_input_tools_paused: bool = false
 @export var is_lighting_enabled: bool = true
+
+# Internal variables
+var _started_at_ticks_msec: int           = 0
+var _spawn_next_gem_at_msec: int          = 0
+var _spawn_next_pickup_at_msec: int       = 0
+var _gem_dont_spawn_until_ticks_msec: int = 0
 
 
 # Check if the player can launch a projectile
@@ -78,8 +81,9 @@ func do_player_goal(player_num: int) -> void:
 	player_score[player_num] = clamp(player_score[player_num] + Constant.PLAYER_SCORE_GOAL_VALUE, 0, Constant.PLAYER_SCORE_VICTORY)
 	print("[GAME] Player %d scored a goal, new score: %d" % [player_num, player_score[player_num]])
 	player_score_updated.emit()
+	_gem_dont_spawn_until_ticks_msec = Time.get_ticks_msec() + Constant.GEM_SPAWN_AFTER_SCORING_DELAY_MSEC
 	if not _check_for_game_over():
-		goal.emit(player_num)
+		show_banner.emit(player_num, Constant.BANNER_TEXT_GOAL, "")
 
 
 # Pause the player input
@@ -107,21 +111,17 @@ func _init() -> void:
 
 
 func _ready() -> void:
-	gem_spawned.connect(_check_for_game_over)
-	pickup_spawned.connect(_check_for_game_over)
 	player_did_collect_item.connect(_on_player_did_collect_item)
 	player_did_launch_projectile.connect(_on_player_launch_projectile)
 	player_enabled.connect(_on_player_enabled)
 	player_energy_updated.connect(_on_player_energy_updated)
 	player_ready_updated.connect(_on_player_ready_updated)
 	player_score_updated.connect(_check_for_game_over)
-	projectile_count_updated.connect(_check_for_game_over)
-	projectile_count_updated.connect(_on_projectile_count_updated)
-	reset_game.connect(_do_reset_game)
+	start_new_game.connect(_do_start_new_game)
 	show_debug_text.connect(_on_show_debug_text)
 
 
-func _do_reset_game() -> void:
+func _do_start_new_game() -> void:
 	unpause_input()
 	is_over = false
 	player_score[1] = Constant.PLAYER_SCORE_INITIAL
@@ -135,6 +135,27 @@ func _do_reset_game() -> void:
 	print("[GAME] Resetting player inventory to: ", player_inventory)
 	player_score_updated.emit()
 	player_inventory_updated.emit()
+	_started_at_ticks_msec = Time.get_ticks_msec()
+	_spawn_next_gem_at_msec = _started_at_ticks_msec + int(Constant.SHOW_MODAL_SEC * 1000)
+	_spawn_next_pickup_at_msec = _spawn_next_gem_at_msec + int(Constant.PICKUP_SPAWN_INITIAL_SEC * 1000)
+	# Countdown and then start the game
+	Game.pause_input()
+	show_banner.emit(0, "READY...", "SET...")
+	await Util.delay(Constant.SHOW_MODAL_SEC)
+	Game.unpause_input()
+
+
+# Called at a fixed rate. 'delta' is the elapsed time since the previous frame.
+func _physics_process(_delta: float) -> void:
+	# Check if it's time to spawn a gem
+	if not is_over and Time.get_ticks_msec() >= _spawn_next_gem_at_msec:
+		_spawn_next_gem_at_msec = Time.get_ticks_msec() + int(Constant.GEM_SPAWN_EVERY_SEC * 1000)
+		spawn_gem.emit()
+	# Check if it's time to spawn a pickup
+	if not is_over and Time.get_ticks_msec() >= _spawn_next_pickup_at_msec:
+		_spawn_next_pickup_at_msec = Time.get_ticks_msec() + int(Constant.PICKUP_SPAWN_EVERY_SEC * 1000)
+		spawn_pickup.emit(Game.InventoryItemType.PROJECTILE) # FUTURE: other types of pickups
+	pass
 
 
 # Check for game over, e.g. when score or gem count is updated
@@ -181,18 +202,22 @@ func _check_for_game_over() -> bool:
 func _do_game_over(result: Result) -> void:
 	if is_over:
 		return
-	pause_input()	
+	pause_input()
 	is_over = true
-	over.emit(result)
+	match result:
+		Game.Result.PLAYER_1_WINS:
+			show_banner.emit(1, Constant.BANNER_TEXT_VICTORY, "")
+		Game.Result.PLAYER_2_WINS:
+			show_banner.emit(2, Constant.BANNER_TEXT_VICTORY, "")
+		Game.Result.DRAW:
+			show_banner.emit(0, Constant.BANNER_TEXT_DRAW, "")
+	await Util.delay(Constant.SHOW_MODAL_SEC)
+	finished.emit()
 
 
 func _on_player_launch_projectile(player_num: int) -> void:
 	_player_inventory_remove(player_num, InventoryItemType.PROJECTILE)
 	player_inventory_updated.emit()
-
-
-func _on_projectile_count_updated() -> void:
-	print ("[PROJECTILES] in play: ", projectiles_in_play)
 
 
 func _on_player_ready_updated() -> void:

models/explosive/projectile_explosive.gd
@@ -20,20 +20,9 @@ func _ready() -> void:
 	# Connect the Collision to the on-collision function
 	connect("body_entered", _on_body_entered)
 
-	# Count this projectile
-	Game.projectiles_in_play += 1
-	Game.projectile_count_updated.emit()
-
 	# Disable lighting if not enabled in settings
 	if not Game.is_lighting_enabled:
 		$PointLight2D.enabled = false
-
-
-# Called when the projectile is removed from the stage
-func _exit_tree() -> void:
-	Game.projectiles_in_play -= 1
-	Game.projectile_count_updated.emit()
-	pass
 
 
 # Called when another body enters the collission area

commit 1cd4c4aa1b03f0797987399b1f8254190af5cefd

Fix "ready.. set.." banner display
Follows regression from #206

scenes/play_game.gd
@@ -22,9 +22,7 @@ func _ready() -> void:
 	_setup()
 	# Create the board before resetting the game (so that scores update on the board)
 	_create_board()
-	# Reset the game
-	Game.start_new_game.emit()
-	# Connect the game over signals after resetting the game
+	# Connect the game signals
 	Game.player_enabled.connect(_on_player_enabled)
 	Game.finished.connect(_on_finished)
 	Game.spawn_gem.connect(_on_spawn_gem)
@@ -36,6 +34,8 @@ func _ready() -> void:
 	else:
 		debug_text.hide()
 	pass
+	# Start a new game
+	Game.start_new_game.emit()
 
 
 # Setup the UI based on the current input mode		