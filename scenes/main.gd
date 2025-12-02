7cd062623d4158fab246b6b22d7f65edb464141f
Charney Kaye <charneykaye@gmail.com>
2025-12-01 21:44:04 -0800

    failsies

--- scenes/main.gd
+++ scenes/main.gd
@@ -9,6 +9,11 @@ func _ready() -> void:
 	Game.player_ready_updated.connect(_on_player_ready_updated)
 	if Game.is_table_mode():
 		_goto_scene("res://scenes/main_table.tscn")
+	$MenuMain.configure([
+	$MenuMain.create_entry("START", Callable(self, "on_start")),
+	$MenuMain.create_entry("OPTIONS", Callable(self, "on_options")),
+	$MenuMain.create_entry("EXIT", Callable(self, "on_exit")),
+	])
 
 
 # If both players are ready, start the game
@@ -18,8 +23,24 @@ func _on_player_ready_updated() -> void:
 		if $ReadyP1.is_ready and $ReadyP2.is_ready:
 			_goto_scene("res://scenes/play_game.tscn")
 
-		
+
 # Goto a scene, guarding against the condition that the tree has been unloaded since the calling thread arrived here
 func _goto_scene(path: String) -> void:
 	if get_tree():
 		get_tree().change_scene_to_file(path)
+
+
+# Start the game
+func on_start() -> void:
+	_goto_scene("res://scenes/play_game.tscn")
+
+
+# Show the options menu
+func on_options() -> void:
+	_goto_scene("res://scenes/options.gd")
+
+
+# Exit the game
+func on_exit() -> void:
+	get_tree().quit()
+	
